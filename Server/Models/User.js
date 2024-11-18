d16d4b034241f4aed2ae9d269e7ef77a144ecdbd
sramirez124 <forwardemails124@gmail.com>
2024-11-18 15:41:58 -0600

    User and Task Schema added
    Going to test next with get and post request

---
+++ Server/Models/User.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        message: "Username required",
+    },
+    password: {
+        type: String,
+        minLength: 6,
+        maxLength: 20,
+        required: true,
+        message: "Password required",
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        message: "Email required",
+    },
+});
+
+export default model("User", userSchema);
