4317162f973d914d4a4805188d9728cf1b0dc159
sramirez124 <forwardemails124@gmail.com>
2024-11-24 11:41:21 -0600

    Made the due date look more presentable

--- Client/src/Home.jsx
+++ Client/src/Home.jsx
@@ -26,7 +26,8 @@ function Home() {
                         <h3>{task.title}</h3>
                         <p>{task.description}</p>
                         <p>Priority: {task.priority}</p>
-                        <p>Due Date: {task.dueDate}</p>
+                        <p>Due Date: {new Date(task.dueDate).toLocaleDateString()}</p>
+
                     </div>
                 ))
             }
